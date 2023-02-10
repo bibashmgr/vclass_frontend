81b464a6107af90466299bf0b8e0b10da8eaa20e
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-02-10 23:13:53 +0545

    [cleanup]: files

--- src/helpers/apiHandler.ts
+++ src/helpers/apiHandler.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:9999';
 const HEADER = {'Content-Type': 'application/json', "Access-Control-Allow-Origin": BASE_URL};
 
-export const apiHelper = async (methodType: string, endpoint: string, payload?: null | object | FormData, ) => {
+export const apiHandler = async (methodType: string, endpoint: string, payload?: null | object | FormData, ) => {
     return await axios({
         method: methodType,
         baseURL: BASE_URL,
--- src/pages/admin/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-const Dashboard = () => {
-    return (
-        <div className='flex justify-center items-center w-screen h-screen'>
-            <p>This is Dashboard.</p>
-        </div>
-    )
-}
-
-export default Dashboard
