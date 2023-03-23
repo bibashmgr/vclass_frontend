484415342870299b68e6974aa26c28195d1d5138
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-03-23 18:38:33 +0545

    [fix]: icon-btn

--- src/components/global/IconButton.tsx
+++ src/components/global/IconButton.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { IconType } from 'react-icons'
 
-const IconButton = ({ Icon, handleClick }: { Icon: IconType, handleClick: React.MouseEventHandler }) => {
+const IconButton = ({ Icon, title = '', handleClick }: { Icon: IconType, title: string, handleClick: React.MouseEventHandler }) => {
     return (
-        <div onClick={handleClick} className='border border-neutralColor-light rounded-md p-[6px] cursor-pointer'>
+        <div title={title} onClick={handleClick} className='border border-neutralColor-light rounded-md p-[6px] cursor-pointer'>
             {<Icon className='w-5 h-5' />}
         </div>
     )
