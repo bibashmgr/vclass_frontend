5b16c4c63b4dff60ee40e7b191064cb23e15edb1
aryalrajan18 <aryalrajan18@gmail.com>
2023-04-25 13:02:59 +0545

    [update]: tableHeaders & schemas

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -3,3 +3,28 @@ export type subjectSchema = {
     name: string,
     codeName: string,
 }
+
+export type facultySchema = {
+    _id: string,
+    name: string,
+    semesters: [],
+}
+
+export type batchSchema = {
+    _id: string,
+    year: string,
+    facultyId: string,
+    semester: number,
+}
+
+export type userSchema = {
+    _id: string,
+    googleId: string,
+    avatar: string,
+    name: string,
+    email: string,
+    role: string,
+    college: string,
+    batch: number,
+    faculty: string,
+}
--- src/utils/tableHeaders.ts
+++ src/utils/tableHeaders.ts
@@ -2,3 +2,22 @@ export const subjectHeader = [
     'Name',
     'Code Name',
 ]
+
+export const facultyHeader = [
+    'Name',
+    'Total Semesters',
+]
+
+export const batchHeader = [
+    'Year',
+    'Faculty',
+    'Total Semesters',
+]
+
+export const userHeader = [
+    'Name',
+    'Email',
+    'Role',
+    'Batch',
+    'Faculty',
+]
