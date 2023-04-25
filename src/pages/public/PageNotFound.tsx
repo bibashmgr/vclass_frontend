ecb400e03b60cb089042d08f1c6a189860487e2c
aryalrajan18 <aryalrajan18@gmail.com>
2023-04-25 13:01:36 +0545

    [update]: 404 page

--- src/pages/public/PageNotFound.tsx
+++ src/pages/public/PageNotFound.tsx
@@ -8,15 +8,17 @@ const PageNotFound = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <div>
-                <p>Oops!</p>
-                <p>You are lost.</p>
+        <div className='flex justify-center items-center w-screen h-screen bg-gray-100 dark:bg-gray-700 px-6'>
+            <div className='max-w-sm md:bg-white dark:md:bg-gray-800 rounded-lg flex flex-col md:gap-2 justify-center items-center md:py-12 md:px-12'>
+                <div className='text-center'>
+                    <p className='text-darkColor dark:text-lightColor font-bold text-2xl'>Oops!</p>
+                    <p className='text-gray-400 dark:text-gray-300 font-normal text-xs'>You are lost.</p>
+                </div>
+                <img src="/images/404.svg" alt="404" className='w-[300px] h-[300px]' />
+                <CustomButton handleClick={() => navigate('/admin')} colorScheme='info'>
+                    Return Home
+                </CustomButton>
             </div>
-            <img src="/images/404.svg" alt="404" />
-            <CustomButton handleClick={() => navigate('/admin')} colorScheme='info'>
-                Return Home
-            </CustomButton>
         </div>
     )
 }
