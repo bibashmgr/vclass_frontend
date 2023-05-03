61043a3813e775dd6a6f59cab6be3ab901cc0140
aryalrajan18 <aryalrajan18@gmail.com>
2023-05-03 08:48:47 +0545

    [add]: modal in userList screen

--- src/pages/admin/user/User.tsx
+++ src/pages/admin/user/User.tsx
@@ -6,6 +6,7 @@ import ListLayout from '../../../layouts/crud_layouts/ListLayout';
 
 // components
 import ActionTd from '../../../components/admin/ActionTd';
+import Modal from '../../../components/global/Modal';
 
 // utils
 import { userSchema } from '../../../utils/schemas';
@@ -13,12 +14,18 @@ import { userHeader } from '../../../utils/tableHeaders';
 
 // handlers
 import { apiHandler } from '../../../handlers/apiHandler';
+import { showMessage } from '../../../handlers/messageHandler';
 
 const User = () => {
     const navigate = useNavigate();
 
     const [users, setUsers] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [updateCounter, setUpdateCounter] = useState<boolean>(false);
+    const [selectedUser, setSelectedUser] = useState<userSchema | null>(
+        null
+    );
 
     const getUsers = async () => {
         await apiHandler('get', 'users', null).then((res) => {
@@ -29,26 +36,58 @@ const User = () => {
         })
     }
 
+    const changeUserStatus = () => {
+        if (selectedUser) {
+            apiHandler('patch', `faculties/status/${selectedUser._id}`).then(
+                (res) => {
+                    if (res.success) {
+                        setIsModalOpen(false);
+                        showMessage(res.message, 'success');
+                        setUpdateCounter(!updateCounter);
+                    } else {
+                        showMessage(res.message, 'failure');
+                    }
+                }
+            );
+        }
+    };
+
     useEffect(() => {
         getUsers();
-    }, [])
+    }, [updateCounter])
 
     return (
-        <ListLayout tableHeader={userHeader} layoutTitle='Users' layoutSubtitle={`${users.length} users added`} isEmpty={users.length === 0} isLoading={isLoading} hasCreateBtn={false}>
-            {
-                users.map((user: userSchema, userIndex) => {
-                    return (
-                        <tr key={user._id} className="bg-lightColor dark:bg-gray-800">
-                            <td className='px-6 py-4'>{userIndex + 1 < 10 ? `0${userIndex + 1}` : userIndex + 1}</td>
-                            <td className='px-6 py-4 capitalize'>{user.name}</td>
-                            <td className='px-6 py-4'>{user.email}</td>
-                            <td className='px-6 py-4 capitalize'>{user.role}</td>
-                            <ActionTd hasView hasEdit hasArchive isHidden={user.isHidden} handleView={() => navigate(`/admin/user/view/${user._id}`)} handleEdit={() => navigate(`/admin/user/edit/${user._id}`)} handleArchive={() => console.log('delete user')} />
-                        </tr>
-                    )
-                })
-            }
-        </ListLayout>
+        <>
+            <Modal
+                isOpen={isModalOpen}
+                handleYes={changeUserStatus}
+                onClose={() => setIsModalOpen(false)}
+                title={
+                    selectedUser?.isHidden
+                        ? 'Are you sure you want to restore this user?'
+                        : 'Are you sure you want to archive this user?'
+                }
+                colorScheme={selectedUser?.isHidden ? 'warn' : 'failure'}
+            />
+            <ListLayout tableHeader={userHeader} layoutTitle='Users' layoutSubtitle={`${users.length} users added`} isEmpty={users.length === 0} isLoading={isLoading} hasCreateBtn={false}>
+                {
+                    users.map((user: userSchema, userIndex) => {
+                        return (
+                            <tr key={user._id} className="bg-lightColor dark:bg-gray-800">
+                                <td className='px-6 py-4'>{userIndex + 1 < 10 ? `0${userIndex + 1}` : userIndex + 1}</td>
+                                <td className='px-6 py-4 capitalize'>{user.name}</td>
+                                <td className='px-6 py-4'>{user.email}</td>
+                                <td className='px-6 py-4 capitalize'>{user.role}</td>
+                                <ActionTd hasView hasEdit hasArchive isHidden={user.isHidden} handleView={() => navigate(`/admin/user/view/${user._id}`)} handleEdit={() => navigate(`/admin/user/edit/${user._id}`)} handleArchive={() => {
+                                    setSelectedUser(user);
+                                    setIsModalOpen(true);
+                                }} />
+                            </tr>
+                        )
+                    })
+                }
+            </ListLayout>
+        </>
     )
 }
 
