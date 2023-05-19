76a02010eaadb539fc8af41c3abe92dd148be36d
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-05-19 23:46:17 +0545

    [fix]: error destructure when messgaeHandling

--- src/handlers/messageHandler.ts
+++ src/handlers/messageHandler.ts
@@ -1,32 +1,69 @@
 import { toast } from 'react-toastify';
 
 // icons
-import { SuccessIcon, FailureIcon, InfoIcon, WarnIcon } from '../components/global/Icons';
+import {
+  SuccessIcon,
+  FailureIcon,
+  InfoIcon,
+  WarnIcon,
+} from '../components/global/Icons';
 
-export const showMessage = (title: string, type: string) => {
-    switch (type) {
-        case 'success':
-            toast.success(title, {
-                icon: SuccessIcon
-            });
-            break;
-    
-        case 'failure':
-            toast.error(title,{
-                icon: FailureIcon
-            });
-            break;
+export const showMessage = (title: string | string[], type: string) => {
+  switch (type) {
+    case 'success':
+      if (Array.isArray(title)) {
+        title.map((t) => {
+          toast.success(t, {
+            icon: SuccessIcon,
+          });
+        });
+      } else {
+        toast.success(title, {
+          icon: SuccessIcon,
+        });
+      }
+      break;
 
-        case 'warn':
-            toast.warn(title,{
-                icon: WarnIcon
-            });
-            break;
+    case 'failure':
+      if (Array.isArray(title)) {
+        title.map((t) => {
+          toast.error(t, {
+            icon: FailureIcon,
+          });
+        });
+      } else {
+        toast.error(title, {
+          icon: FailureIcon,
+        });
+      }
+      break;
 
-        default: 
-            toast.info(title,{
-                icon: InfoIcon
-            });
-            break;
-    }
-}
+    case 'warn':
+      if (Array.isArray(title)) {
+        title.map((t) => {
+          toast.warn(t, {
+            icon: WarnIcon,
+          });
+        });
+      } else {
+        toast.warn(title, {
+          icon: WarnIcon,
+        });
+      }
+      break;
+
+    default:
+      if (Array.isArray(title)) {
+        title.map((t) => {
+          toast.info(t, {
+            icon: InfoIcon,
+          });
+        });
+      } else {
+        toast.info(title, {
+          icon: InfoIcon,
+        });
+      }
+      break;
+  }
+};
