d3b677e91f903fe870f4a5f21dfc906da9cdffe3
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-06-14 18:11:58 +0545

    [update]: useAuth hook

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -8,20 +8,21 @@ import { apiHandler } from '../handlers/apiHandler';
 import { userSchema } from '../utils/schemas';
 
 // context
-// import { useUserInfo } from '../context/UserInfoContext';
+import { useUserInfo } from '../context/UserInfoContext';
 
 export const useAuth = () => {
   const [token, setToken] = useState('');
   const [user, setUser] = useState<userSchema | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  // const userInfo = useUserInfo();
+  const userInfoContext = useUserInfo();
 
   const getUser = async () => {
     const res = await apiHandler('get', 'auth/login/success');
 
     if (res.success) {
       setUser(res.data);
+      userInfoContext?.setUserInfo(res.data);
     }
     setIsLoading(false);
   };
