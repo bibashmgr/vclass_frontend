c528859254c596e5dddca687db5bf2200ea8b0e1
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-06-14 18:11:18 +0545

    [add]: userContext Provider

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,12 @@ import './index.css';
 
 // context
 import { ThemeProvider } from './context/ThemeContext';
-// import { UserInfoProvider } from './context/UserInfoContext';
+import { UserInfoProvider } from './context/UserInfoContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ThemeProvider>
-    {/* <UserInfoProvider> */}
-    <App />
-    {/* </UserInfoProvider> */}
+    <UserInfoProvider>
+      <App />
+    </UserInfoProvider>
   </ThemeProvider>
 );
