a50f5046f0db908a84b7677d05625ce461c05442
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-07-13 23:01:52 +0545

    [add]: extra-props for popover

--- src/components/global/Popover.tsx
+++ src/components/global/Popover.tsx
@@ -3,9 +3,14 @@ import React, { useRef, useState, useEffect } from 'react';
 type propsType = {
   parentElement: React.ReactNode;
   children: React.ReactNode;
+  popDirection?: 'left' | 'right';
 };
 
-const Popover = ({ parentElement, children }: propsType) => {
+const Popover = ({
+  parentElement,
+  children,
+  popDirection = 'right',
+}: propsType) => {
   const popoverRef = useRef<HTMLDivElement>(null);
   const [isPopover, setIsPopover] = useState(false);
 
@@ -32,17 +37,23 @@ const Popover = ({ parentElement, children }: propsType) => {
   }, []);
 
   return (
-    <div className="relative" ref={popoverRef}>
+    <div className='relative' ref={popoverRef}>
       <div onClick={handlePopover}>{parentElement}</div>
       <div
-        className={`absolute z-10 right-0 top-12 inline-block w-44 divide-y divide-gray-100 dark:divide-gray-600 text-sm text-gray-500 dark:text-gray-400 transition-opacity duration-300 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-600 rounded-md shadow-light dark:shadow-none ${
+        className={`absolute z-10 ${
+          popDirection === 'right' ? 'right-0' : 'left-0'
+        } top-12 inline-block min-w-[120px] divide-y divide-gray-100 dark:divide-gray-600 text-sm text-gray-500 dark:text-gray-400 transition-opacity duration-300 bg-white dark:bg-slate-800 border border-gray-200 dark:border-slate-600 rounded-md shadow-light dark:shadow-none ${
           isPopover
             ? 'opacity-100 cursor-pointer pointer-events-auto'
             : 'opacity-0 cursor-none pointer-events-none'
         }`}
       >
         {children}
-        <div className="w-3.5 h-3.5 bg-white dark:bg-gray-800 border-l border-t border-gray-200 dark:border-gray-600 absolute -top-2 right-2.5 rotate-45"></div>
+        <div
+          className={`w-3.5 h-3.5 bg-white dark:bg-gray-800 border-l border-t border-gray-200 dark:border-gray-600 absolute -top-2 ${
+            popDirection === 'right' ? 'right-2.5' : 'left-2.5'
+          } rotate-45`}
+        ></div>
       </div>
     </div>
   );
