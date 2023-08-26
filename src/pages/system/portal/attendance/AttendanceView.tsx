4510f9f7473b21e77f56b0d169974ba623d21927
aryalrajan18 <aryalrajan18@gmail.com>
2023-08-26 21:37:29 +0545

    [fix]: some changes

--- src/pages/system/portal/attendance/AttendanceView.tsx
+++ src/pages/system/portal/attendance/AttendanceView.tsx
@@ -41,6 +41,7 @@ const AttendanceView = () => {
     );
 
     if (res.success) {
+      console.log(res.data);
       setStats(res.data);
       setIsLoading(false);
     }
@@ -53,63 +54,50 @@ const AttendanceView = () => {
   return (
     <>
       {isLoading ? (
-        <div className="flex justify-center py-8">
+        <div className='flex justify-center py-8'>
           <Spinner boxSize={5} />
         </div>
       ) : (
-        <div className="flex flex-col gap-4 py-4">
+        <div className='flex flex-col gap-4 py-4'>
           {userInfoContext?.userInfo?.role === 'teacher' && (
-            <div className="flex items-center gap-4 bg-white dark:bg-gray-800 px-6 py-6 rounded-lg">
-              <img src={stats?.user.avatar} className="rounded-lg w-12 h-12" />
-              <div className="flex flex-col">
-                <p className="text-gray-900 dark:text-white font-bold text-lg">
+            <div className='flex items-center gap-4 bg-white dark:bg-gray-800 px-6 py-6 rounded-lg'>
+              <img src={stats?.user.avatar} className='rounded-lg w-12 h-12' />
+              <div className='flex flex-col'>
+                <p className='text-gray-900 dark:text-white font-bold text-lg'>
                   {stats?.user.name}
                 </p>
-                <p className="text-gray-400 font-semibold text-xs">
+                <p className='text-gray-400 font-semibold text-xs'>
                   {stats?.user.email}
                 </p>
               </div>
             </div>
           )}
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
             <StatusCard
               Icon={TiGroup}
-              colorScheme="success"
-              title={
-                stats?.activeDates.length
-                  ? numberTrimmer(stats?.activeDates.length)
-                  : '00'
-              }
-              subtitle="Total Present Days"
+              colorScheme='success'
+              title={numberTrimmer(stats?.activeDates.length!)}
+              subtitle='Total Present Days'
             />
             <StatusCard
               Icon={TiGroup}
-              colorScheme="failure"
-              title={
-                stats?.portal.activeDates.length && stats?.activeDates.length
-                  ? numberTrimmer(
-                      stats?.portal?.activeDates.length! -
-                        stats?.activeDates.length!
-                    )
-                  : '00'
-              }
-              subtitle="Total Absent Days"
+              colorScheme='failure'
+              title={numberTrimmer(
+                stats?.portal?.activeDates.length! - stats?.activeDates.length!
+              )}
+              subtitle='Total Absent Days'
             />
             <StatusCard
               Icon={TiGroup}
-              colorScheme="info"
-              title={
-                stats?.portal.activeDates.length && stats?.activeDates.length
-                  ? `${numberTrimmer(
-                      Math.floor(
-                        (stats?.activeDates.length! /
-                          stats?.portal?.activeDates.length!) *
-                          100
-                      )
-                    )}%`
-                  : '0%'
-              }
-              subtitle="Present Percentage"
+              colorScheme='info'
+              title={`${numberTrimmer(
+                Math.floor(
+                  (stats?.activeDates.length! /
+                    stats?.portal?.activeDates.length!) *
+                    100
+                )
+              )}%`}
+              subtitle='Present Percentage'
             />
           </div>
         </div>
