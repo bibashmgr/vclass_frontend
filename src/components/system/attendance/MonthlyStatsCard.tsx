f7e8a6dd98f467a2bf9f162d342184394ee3da72
aryalrajan18 <aryalrajan18@gmail.com>
2023-08-27 23:05:18 +0545

    [add]: loader for bar chart

--- src/components/system/attendance/MonthlyStatsCard.tsx
+++ src/components/system/attendance/MonthlyStatsCard.tsx
@@ -3,6 +3,7 @@ import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 
 // schemas
 import { userAttendanceStatsSchema } from '../../../utils/schemas';
+import Spinner from '../../global/Spinner';
 
 let months = [
   'Jan',
@@ -30,9 +31,30 @@ type monthlyStatsSchema = {
 };
 
 const MonthlyStatsCard = ({ stats }: propsType) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [monthlyStats, setMonthlyStats] = useState<monthlyStatsSchema[]>([]);
 
-  useEffect(() => {
+  const groupDates = (dates: Date[]) => {
+    const groupedDates: any = {};
+
+    dates.forEach((d) => {
+      const dt = new Date(d);
+      const date = dt.getDate();
+      const year = dt.getFullYear();
+      const month = dt.getMonth() + 1;
+
+      const key = `${year}-${month}`;
+      if (key in groupedDates) {
+        groupedDates[key] = [...groupedDates[key], date];
+      } else {
+        groupedDates[key] = [date];
+      }
+    });
+
+    return groupedDates;
+  };
+
+  const setStats = () => {
     let statsData: monthlyStatsSchema[] = [];
     let sortedTotalDates = stats.portal.activeDates.sort(
       (a, b) => new Date(a).getTime() - new Date(b).getTime()
@@ -79,27 +101,12 @@ const MonthlyStatsCard = ({ stats }: propsType) => {
       });
 
     setMonthlyStats(statsData);
-  }, []);
-
-  const groupDates = (dates: Date[]) => {
-    const groupedDates: any = {};
-
-    dates.forEach((d) => {
-      const dt = new Date(d);
-      const date = dt.getDate();
-      const year = dt.getFullYear();
-      const month = dt.getMonth() + 1;
-
-      const key = `${year}-${month}`;
-      if (key in groupedDates) {
-        groupedDates[key] = [...groupedDates[key], date];
-      } else {
-        groupedDates[key] = [date];
-      }
-    });
-
-    return groupedDates;
+    setIsLoading(false);
   };
+
+  useEffect(() => {
+    setStats();
+  }, []);
 
   return (
     <div className='flex flex-col gap-4 items-start justify-start bg-white dark:bg-gray-800 px-6 py-6 rounded-lg'>
@@ -112,43 +119,122 @@ const MonthlyStatsCard = ({ stats }: propsType) => {
         </p>
       </div>
       <div className='w-full h-[300px] lg:h-[400px]'>
-        <ResponsiveContainer>
-          <BarChart data={monthlyStats} margin={{ left: -32 }} barGap={0}>
-            <XAxis
-              dataKey='name'
-              axisLine={false}
-              tickLine={false}
-              fontSize={12}
-              fontWeight='bold'
-              fontFamily='Open Sans'
-              padding={{ left: 16, right: 16 }}
-            />
-            <YAxis
-              axisLine={false}
-              tickLine={false}
-              fontSize={12}
-              fontWeight='bold'
-              fontFamily='Open Sans'
-              padding={{ top: 16, bottom: 16 }}
-              domain={[0, 100]}
-            />
-            <Bar
-              dataKey='presence'
-              fill='#6EE7B7'
-              barSize={12}
-              radius={[4, 4, 0, 0]}
-            />
-            <Bar
-              dataKey='absence'
-              fill='#F87171'
-              barSize={12}
-              radius={[4, 4, 0, 0]}
-            />
-          </BarChart>
-        </ResponsiveContainer>
+        {isLoading ? (
+          <BarChartSkeleton />
+        ) : (
+          <ResponsiveContainer>
+            <BarChart data={monthlyStats} margin={{ left: -32 }} barGap={0}>
+              <XAxis
+                dataKey='name'
+                axisLine={false}
+                tickLine={false}
+                fontSize={12}
+                fontWeight='bold'
+                fontFamily='Open Sans'
+                padding={{ left: 16, right: 16 }}
+              />
+              <YAxis
+                axisLine={false}
+                tickLine={false}
+                fontSize={12}
+                fontWeight='bold'
+                fontFamily='Open Sans'
+                padding={{ top: 16, bottom: 16 }}
+                domain={[0, 100]}
+              />
+              <Bar
+                dataKey='presence'
+                fill='#6EE7B7'
+                barSize={12}
+                radius={[4, 4, 0, 0]}
+              />
+              <Bar
+                dataKey='absence'
+                fill='#F87171'
+                barSize={12}
+                radius={[4, 4, 0, 0]}
+              />
+            </BarChart>
+          </ResponsiveContainer>
+        )}
       </div>
     </div>
   );
 };
 
 export default MonthlyStatsCard;
+
+const BarChartSkeleton = () => {
+  return (
+    <div className='flex w-full h-full gap-4'>
+      <ul className='flex flex-col justify-between list-none text-gray-500 font-bold text-xs pb-8'>
+        <li>100</li>
+        <li>75</li>
+        <li>50</li>
+        <li>25</li>
+        <li>0</li>
+      </ul>
+
+      <div className='w-full h-full flex flex-col gap-4'>
+        <div className='animate-pulse flex w-full h-full justify-between'>
+          <div className='flex items-end'>
+            <div
+              className={`h-5/6 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+            <div
+              className={`h-1/6 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+          </div>
+          <div className='flex items-end'>
+            <div
+              className={`h-4/6 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+            <div
+              className={`h-2/6 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+          </div>
+          <div className='flex items-end'>
+            <div
+              className={`h-2/5 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+            <div
+              className={`h-3/5 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+          </div>
+          <div className='flex items-end'>
+            <div
+              className={`h-1/6 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+            <div
+              className={`h-5/6 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+          </div>
+          <div className='flex items-end'>
+            <div
+              className={`h-2/3 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+            <div
+              className={`h-1/3 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+          </div>
+          <div className='flex items-end'>
+            <div
+              className={`h-1/4 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+            <div
+              className={`h-3/4 bg-gray-200 rounded-t dark:bg-gray-700 w-4`}
+            />
+          </div>
+        </div>
+        <ul className='flex justify-between list-none text-gray-500 font-bold text-xs'>
+          <li>A</li>
+          <li>B</li>
+          <li>C</li>
+          <li>D</li>
+          <li>E</li>
+          <li>F</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
